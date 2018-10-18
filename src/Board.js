3d647ae792d1cd8b6ddebab6c93b815c71ca03e0
elemee3 <leila.m.elshamy@gmail.com>
2018-10-17 20:54:03 -0700

    added game over functionality

--- src/Board.js
+++ src/Board.js
@@ -6,14 +6,65 @@ class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: ['', '', '', '', '', '', '', '', '']
+      clickCount: 0,
+      board: ['', '', '', '', '', '', '', '', ''],
+      winList: [[0,1,2], [3,4,5], [6,7,8],
+                [0,3,6], [1,4,7], [2,5,8],
+                [0,4,8], [2,4,6]]
     }
   }
+
+  handleAllClicks = (id, player) => {
+    let { clickCount, board } = this.state
+    clickCount++
+    board[id] = player
+    this.setState({
+      clickCount: clickCount,
+      board: board
+    })
+    if (this.isWin() || this.isLoss()) {
+      this.handleGameOver(player)
+    }
+  }
+
+  isWin = () => {
+    let { board, winList } = this.state
+    let isWin = false
+    //if the board matches a combo in winList, return true
+    for (let i = 0; i < winList.length; i++) {
+      if (board[winList[i][0]] !== '' &&
+          board[winList[i][0]] === board[winList[i][1]] &&
+          board[winList[i][1]] === board[winList[i][2]]) {
+        isWin = true
+        return isWin
+      } else {
+        isWin = false
+      }
+    }
+    return isWin
+  }
+
+  isLoss = () => {
+    if (this.state.clickCount > 7) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  handleGameOver = (player) => {
+    if (this.isWin()) {
+      alert(`Game over, ${player} wins!`)
+    } else {
+      alert(`Game over, losers`)
+    }
+  }
+
   render() {
     let { board } = this.state
     let boxes = board.map((box, index) => {
       return (
-        <Box contents={box} id={index} />
+        <Box contents={box} id={index} onAllClicks={this.handleAllClicks} count={this.state.clickCount} />
       )
     })
     return (
--- src/Box.js
+++ src/Box.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Box extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      player: '',
+      isActive: true
+    }
+  }
+
+  handleClick = () => {
+    let { player, isActive } = this.state
+    if (isActive) {                         // only do stuff with active boxes
+      if (this.props.count % 2 === 0) {
+        player = 'X'                        // change player val to x
+        this.props.onAllClicks(this.props.id, player)
+        this.setState({
+          player: player,
+          isActive: false                   // deactivate box
+        })
+      } else {
+        player = 'O'                        // change player val to o
+        this.props.onAllClicks(this.props.id, player)
+        this.setState({
+          player: player,
+          isActive: false                   // deactivate box
+        })
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="Box">
-        Inside box
+      <div className="Box" onClick={this.handleClick} key={this.props.id} >
+        {this.state.player}
       </div>
     );
   }
